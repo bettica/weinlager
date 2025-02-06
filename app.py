3b68793f3b89a6b02742cbb368e81b34f99c003d
bettica <carla.betti@outlook.com>
2025-02-06 21:56:16 +0100

    Update app.py
    Link zur Kaufe auf Website geändert

--- app.py
+++ app.py
@@ -251,7 +251,7 @@ def main():
                  jahrgang = st.text_input("Jahrgang")
                  lagerort = st.text_input("Lagerort")
                  preis_pro_einheit = st.number_input("Preis pro Einheit")
-                 kauf_link = st.text_input("Kauf-Link")
+                 kauf_link = st.text_input("Website")
     
                  if st.button("Produkt registrieren"):
                      register_product(weingut, rebsorte, lage, land, jahrgang, lagerort, preis_pro_einheit, kauf_link)
@@ -287,7 +287,7 @@ def main():
                     ORDER BY 2
                     '''
                  df = pd.read_sql(query, conn)
-                 df.columns = ["PRODUKTNR", "WEINGUT", "REBSORTE", "LAGE", "LAND", "JAHRGANG", "LAGERORT", "BESTANDMENGE", "EINZELPREIS", "GESAMTPREIS", "LINK ZUR KAUF"]
+                 df.columns = ["PRODUKTNR", "WEINGUT", "REBSORTE", "LAGE", "LAND", "JAHRGANG", "LAGERORT", "BESTANDMENGE", "EINZELPREIS", "GESAMTPREIS", "WEBSITE"]
                  conn.close()
                  st.dataframe(df)
             
